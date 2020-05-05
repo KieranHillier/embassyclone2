c1f4bc6464f91a692fb3bb128cb77bdb4fa5baec
Kieran Hillier <kieran@Kierans-MacBook-Pro.local>
2020-05-05 08:45:19 -0400

    Updates

---
+++ precache-manifest.630950a99e483a7207483734bb6c6b69.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc03339891a39dc2dedda7766f0140e",
+    "url": "/embassyclone2/index.html"
+  },
+  {
+    "revision": "98037b0abac6fadaad6b",
+    "url": "/embassyclone2/static/css/2.d4afeea5.chunk.css"
+  },
+  {
+    "revision": "eeb8b2ec2b76f055365f",
+    "url": "/embassyclone2/static/css/main.aad4844c.chunk.css"
+  },
+  {
+    "revision": "98037b0abac6fadaad6b",
+    "url": "/embassyclone2/static/js/2.22e575ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/embassyclone2/static/js/2.22e575ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb8b2ec2b76f055365f",
+    "url": "/embassyclone2/static/js/main.6aecebd2.chunk.js"
+  },
+  {
+    "revision": "2ecf01aa804047a25a23",
+    "url": "/embassyclone2/static/js/runtime-main.034cb205.js"
+  },
+  {
+    "revision": "299eb7ae8b34b20269f0f81f67f954cc",
+    "url": "/embassyclone2/static/media/call-ico.299eb7ae.svg"
+  },
+  {
+    "revision": "2e152f8f0aac4e2faf1e3c690cbe2df9",
+    "url": "/embassyclone2/static/media/home-banner-1.2e152f8f.jpg"
+  },
+  {
+    "revision": "5a66856f64f63ed4af55da000808a5f0",
+    "url": "/embassyclone2/static/media/home-banner-2.5a66856f.jpg"
+  },
+  {
+    "revision": "b012b4a6e03a6bad461ec669631159bb",
+    "url": "/embassyclone2/static/media/home-banner-3.b012b4a6.jpg"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/embassyclone2/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "aba639b3859ad62015ee339bacbee66b",
+    "url": "/embassyclone2/static/media/mobile-menu.aba639b3.svg"
+  },
+  {
+    "revision": "00f6172946a2b8eddcf0665332d33946",
+    "url": "/embassyclone2/static/media/msg-ico.00f61729.svg"
+  }
+]);
