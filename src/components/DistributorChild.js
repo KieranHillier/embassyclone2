87230c45674a6983c9adc66844328d4dd829a2de
Kieran Hillier <kieranhillier@outlook.com>
2020-07-20 16:32:01 -0400

    finished distributors page

--- src/components/DistributorChild.js
+++ src/components/DistributorChild.js
@@ -14,15 +14,17 @@ export default class DistributorChild extends Component {
         return (
             <div className='distributors-child-container'>
                 <div className='distributors-child-header' onClick={() => this.setState({ opened: !opened })}>
-                    <h2>{element.title}</h2>
+                    <h2 style={opened ? {color: '#2ea948'} : null}>{element.title}</h2>
                     <img className='distributors-icon' alt='' src={DropdownArrow} style={!opened ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
                 </div>
                 { opened ? (
                     <div className='distributors-child-body'>
                         <p>{element.address}</p>
                         <p>{element.number}</p>
-                        <p>{element.fax}</p>
-                        <a href={`https://${element.website}`} target='_blank' rel='noopener noreferrer'>{element.website}</a>
+                        {element.fax ? <p>{element.fax}</p> : null}
+                        {element.toll ? <p>{element.toll}</p> : null}
+                        {element.email ? <p>{element.email}</p> : null}
+                        {element.website ? <a href={`https://${element.website}`} target='_blank' rel='noopener noreferrer'>{element.website}</a> : null}                    
                     </div>
                 ) : null}
             </div>
--- src/screens/Distributors.js
+++ src/screens/Distributors.js
@@ -3,6 +3,7 @@ import DropdownArrow from '../images/dropdownArrow.svg';
 import './Distributors.css';
 import DistributorsData from '../data/distributors.json';
 import DistributorChild from '../components/DistributorChild';
+import { Link } from 'react-router-dom';
 
 class Distributors extends Component {
 
@@ -12,6 +13,9 @@ class Distributors extends Component {
       parents: {
         canada: false,
         usa: false,
+        mexico: false,
+        central_america: false,
+        middle_east_and_asia: false,
       },
       provinces: {
         ontario: false,
@@ -48,59 +52,82 @@ class Distributors extends Component {
     const { parents, provinces } = this.state
     console.log(DistributorsData)
     return (
-      <div className='distributors-container'>
-          <div className='distributors-header-image'></div>
-          <div className='distributors-content-container'>
-              <div className='distributors-header-text'>
-                <h1>Distributors</h1>
-                <p>Embassy Ingredients has customers all over the world and we work with some of the largest and most recognized names in the food service and food product manufacturing inustries. We also server a wide range of bakeries and food processors both direct and through out Distributor and Agents partners.</p>
-              </div>
-              <div className='distributors-data-container'>
-                <div className='distributors-parent' onClick={() => this._updateParent('canada')}>
-                  <h2>Canada</h2>
-                  <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.canada ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+      <>
+        <div className='distributors-container'>
+            <div className='distributors-header-image'></div>
+            <div className='distributors-content-container'>
+                <div className='distributors-header-text'>
+                  <h1>Distributors</h1>
+                  <p>Embassy Ingredients has customers all over the world and we work with some of the largest and most recognized names in the food service and food product manufacturing inustries. We also server a wide range of bakeries and food processors both direct and through out Distributor and Agents partners.</p>
                 </div>
-                {parents.canada ? ( 
-                  <>
-                    <div className='distributors-province' onClick={() => this._updateProvince('ontario')}>
-                      <h2>Ontario</h2>
-                      <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.ontario ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                <div className='distributors-data-container'>
+                  <div className='distributors-parent' onClick={() => this._updateParent('canada')}>
+                    <h2 style={parents.canada ? {color: '#2ea948'} : null}>Canada</h2>
+                    <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.canada ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                  </div>
+                  {parents.canada ? ( 
+                    <>
+                      <div className='distributors-province' onClick={() => this._updateProvince('ontario')}>
+                        <h2 style={provinces.ontario ? {color: '#2ea948'} : null}>Ontario</h2>
+                        <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.ontario ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                      </div>
+                      {provinces.ontario ? this._renderChildren('ontario') : null}
+                      <div className='distributors-province' onClick={() => this._updateProvince('quebec')}>
+                        <h2 style={provinces.quebec ? {color: '#2ea948'} : null}>Quebec</h2>
+                        <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.quebec ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                      </div>
+                      {provinces.quebec ? this._renderChildren('quebec') : null}
+                      <div className='distributors-province' onClick={() => this._updateProvince('western')}>
+                        <h2 style={provinces.western ? {color: '#2ea948'} : null}>Western Canada</h2>
+                        <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.western ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                      </div>
+                      {provinces.western ? this._renderChildren('western') : null}
+                      <div className='distributors-province' onClick={() => this._updateProvince('atlantic')}>
+                        <h2 style={provinces.atlantic ? {color: '#2ea948'} : null}>Atlantic Canada</h2>
+                        <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.atlantic ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                      </div>
+                      {provinces.atlantic ? this._renderChildren('atlantic') : null}
+                    </>
+                  ) : null} 
+                  <div className='distributors-usa-container'>
+                    <div className='distributors-usa' onClick={() => this._updateParent('usa')}>
+                      <h2 style={parents.usa ? {color: '#2ea948'} : null}>United States of America</h2>
+                      <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.usa ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
                     </div>
-                    {provinces.ontario ? this._renderChildren('ontario') : null}
-                    <div className='distributors-province' onClick={() => this._updateProvince('quebec')}>
-                      <h2>Quebec</h2>
-                      <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.quebec ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
-                    </div>
-                    {provinces.quebec ? this._renderChildren('quebec') : null}
-                    <div className='distributors-province' onClick={() => this._updateProvince('western')}>
-                      <h2>Western Canada</h2>
-                      <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.western ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
-                    </div>
-                    <div className='distributors-province' onClick={() => this._updateProvince('atlantic')}>
-                      <h2>Atlantic Canada</h2>
-                      <img className='distributors-icon' alt='' src={DropdownArrow} style={!provinces.atlantic ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
-                    </div>
-                  </>
-                ) : null} 
-                <div className='distributors-parent' onClick={() => this._updateParent('usa')}>
-                  <h2>United States of America</h2>
-                  <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.usa ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                    {parents.usa ? (
+                      <div className='distributors-usa-text'>
+                        <h3>Want to work with Embassy in the United States?</h3>
+                        <p>If you are interested in using Embassy Ingredients for your business in the U.S., please contact us directly and find our more information about the industries we serve, our products, finished concepts, and terms of sale.</p>
+                        <div className='distributors-usa-btn'>
+                          <Link to="/contact">Contact Us</Link>
+                        </div>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div className='distributors-parent' onClick={() => this._updateParent('mexico')}>
+                    <h2 style={parents.mexico ? {color: '#2ea948'} : null}>Mexico</h2>
+                    <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.mexico ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                  </div>
+                  {parents.mexico ? this._renderChildren('mexico') : null}
+                  <div className='distributors-parent' onClick={() => this._updateParent('central_america')}>
+                    <h2 style={parents.central_america ? {color: '#2ea948'} : null}>Central America</h2>
+                    <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.central_america ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                  </div>
+                  {parents.central_america ? this._renderChildren('central_america') : null}
+                  <div className='distributors-parent' onClick={() => this._updateParent('middle_east_and_asia')}>
+                    <h2 style={parents.middle_east_and_asia ? {color: '#2ea948'} : null}>Middle East &amp; South East Asia</h2>
+                    <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.middle_east_and_asia ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
+                  </div>
+                  {parents.middle_east_and_asia ? this._renderChildren('middle_east_and_asia') : null}
                 </div>
-                <div className='distributors-parent' onClick={() => this._updateParent('usa')}>
-                  <h2>Mexico</h2>
-                  <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.usa ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
-                </div>
-                <div className='distributors-parent' onClick={() => this._updateParent('usa')}>
-                  <h2>Central America</h2>
-                  <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.usa ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
-                </div>
-                <div className='distributors-parent' onClick={() => this._updateParent('usa')}>
-                  <h2>Middle East &amp; South East Asia</h2>
-                  <img className='distributors-icon' alt='' src={DropdownArrow} style={!parents.usa ? {transform: 'rotate(90deg)'} : {transform: 'rotate(180deg)'}} />
-                </div>
-              </div>
+            </div>
+        </div>
+        <div className='distributors-bottom-container'>
+          <div className='distributors-content'>
+            <p>If you are interested in using Embassy Ingredients for your business, please contact us for more information about the industries we serve, our products, finished concepts, and terms of sale.</p>
           </div>
-      </div>
+        </div>
+      </>
     );
   }
 }
